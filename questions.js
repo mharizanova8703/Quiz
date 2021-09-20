d706b52c2f697a69c74012fa2a57319a6d61b9fc
mharizanova8703 <mharizanova@yahoo.com>
2021-09-20 10:21:46 -0400

    updating questions
    adding second  question

--- questions.js
+++ questions.js
@@ -4,4 +4,9 @@ var questions = [
     choices: ['strings', 'alerts', 'numbers', ' template'],
     answer: '  template',
   },
+  {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
 ]
