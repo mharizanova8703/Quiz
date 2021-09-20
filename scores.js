843a1dd8990300d4a1e2e3838fe6e0993ab70d09
mharizanova8703 <mharizanova@yahoo.com>
2021-09-20 14:27:25 -0400

    updating  scores.js
    adding local.storage
    loop

--- scores.js
+++ scores.js
@@ -6,3 +6,18 @@ clear.addEventListener('click', function () {
   localStorage.clear()
   location.reload()
 })
+//adding local storage
+var allScores = localStorage.getItem('allScores')
+allScores = JSON.parse(allScores)
+
+if (allScores !== null) {
+  for (var i = 0; i < allScores.length; i++) {
+    var createLi = document.createElement('li')
+    createLi.textContent = allScores[i].initials + ' ' + allScores[i].score
+    highScore.appendChild(createLi)
+  }
+}
+// Event listener to move to index page
+goBack.addEventListener('click', function () {
+  window.location.replace('./index.html')
+})
