73924ac5182a5303921259bc59f502886308e387
mharizanova8703 <mharizanova@yahoo.com>
2021-09-20 10:56:23 -0400

    updating questions .js
    adding query.Selector
    #currentTime
    #startTime
    #questionsDiv
    #wrapper

--- questions.js
+++ questions.js
@@ -36,3 +36,17 @@ var questions = [
 //declared variables
 var score = 0
 var questionIndex = 0
+
+var currentTime = document.querySelector('#currentTime')
+var timer = document.querySelector('#startTime')
+var questionsDiv = document.querySelector('#questionsDiv')
+var wrapper = document.querySelector('#wrapper')
+
+// Seconds left is 15 seconds per question:
+var secondsLeft = 76
+// Holds interval time
+var holdInterval = 0
+// Holds penalty time
+var penalty = 10
+// Creates new element
+var ulCreate = document.createElement('ul')
