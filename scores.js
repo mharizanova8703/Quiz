30c7255a313955c45213a09abbe6a1c387510f75
mharizanova8703 <mharizanova@yahoo.com>
2021-09-20 14:22:09 -0400

    Update scores.js
    adding event listener

--- scores.js
+++ scores.js
@@ -1,3 +1,8 @@
 var highScore = document.querySelector('#highScore')
 var clear = document.querySelector('#clear')
 var goBack = document.querySelector('#goBack')
+// adding event listener to clear scores
+clear.addEventListener('click', function () {
+  localStorage.clear()
+  location.reload()
+})
